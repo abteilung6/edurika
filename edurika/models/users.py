27e96ff07639f732f33562b31a3287cf6c7ef89f
Tilman <moeller.tilman@gmail.com>
2025-01-23 20:52:04 +0100

    add user model

---
+++ edurika/models/users.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, DateTime, Integer, String, func
+
+from edurika.models.base_class import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    created_time: Column = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    public_name = Column(String, unique=True, nullable=False)
+    username = Column(String, unique=True, nullable=False)
+    subtitle = Column(String, nullable=True)
+    description = Column(String, nullable=True)
