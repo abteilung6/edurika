80d6a62914a50cafe6c93f5bd04eddd168b2019a
Tilman <moeller.tilman@gmail.com>
2025-01-25 18:19:27 +0100

    set cors for local development

--- edurika/app.py
+++ edurika/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from edurika.api.api import api_router
 
@@ -6,6 +7,13 @@ from edurika.api.api import api_router
 def get_application() -> FastAPI:
     app = FastAPI(
         title="Edurika API",
+    )
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://127.0.0.1:5173", "http://localhost:5173"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
     app.include_router(api_router)
     return app
